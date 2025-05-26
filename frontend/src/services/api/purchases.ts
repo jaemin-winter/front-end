946a624e20c9bd53ef1418b394251fcfa0a0c43f
jm13 <ktd562@naver.com>
2025-05-26 09:22:19 +0900

    refact: 백엔드 리워드 응답 수정에 따른 api 수정 - 백엔드 리워드 응답 수정으로 인해 api 수정하였습니다.

--- frontend/src/services/api/purchases.ts
+++ frontend/src/services/api/purchases.ts
@@ -4,12 +4,12 @@ export const purchasesService = {
   fetchPurchaseList: async () => {
     const response = await apiClient.get('/rewards/list')
 
-    const result = response.data.results
+    const result = response.data
     if (!result || result.status !== 'success') {
       throw new Error('API 응답 실패')
     }
 
-    return result.data
+    return result.data.rewards
   }
 }
 
--- frontend/src/services/api/rewardsService.ts
+++ frontend/src/services/api/rewardsService.ts
@@ -3,11 +3,12 @@ import apiClient from '@/plugins/axios'
 export const rewardsService = {
   fetchRewardList: async () => {
     const response = await apiClient.get('/rewards/')
-    const result = response.data.results
+    const result = response.data
+
     if (!result || result.status !== 'success') {
       throw new Error('API 응답 실패')
     }
-    return result.data
+    return result.data.rewards
   },
   purchaseReward: async (rewardId: number) => {
     try {
